11b435a9d2f596fefb776974be70bd8a7e068db3
Spencriko <sofa.aleksandrina1999@gmail.com>
2024-09-03 03:19:01 +0300

    1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,21 @@
+console.log('File is running.');
+
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+export const fetchPhotos = (searchQuery, page) => {
+  const axiosOptions = {
+    params: {
+      key: '45746393-55b87f9c702251a87b9ae7181',
+      q: searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: page,
+      per_page: 15,
+    },
+  };
+
+  return axios.get('', axiosOptions);
+};
