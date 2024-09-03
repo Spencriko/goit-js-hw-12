ac2b3d153aee5768c22b427d6a5201ac3422d737
Spencriko <sofa.aleksandrina1999@gmail.com>
2024-09-03 13:04:34 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import commonjs from '@rollup/plugin-commonjs';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
@@ -12,25 +11,20 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
+
       rollupOptions: {
         input: glob.sync('./src/*.html'),
-        plugins: [
-          commonjs({
-            include: /node_modules/,
-            transformMixedEsModules: true,
-          }),
-        ],
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
               return 'vendor';
             }
           },
-          entryFileNames: '[name]-[hash].js',
+          entryFileNames: 'commonHelpers.js',
         },
       },
       outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/*.html'])],
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
   };
 });
